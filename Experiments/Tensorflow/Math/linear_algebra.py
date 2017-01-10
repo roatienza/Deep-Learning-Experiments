ff5ec59e34699de75f27ba1883d72f9a0a303a7b
Rowel Atienza <rowel.atienza@gmail.com>
2017-01-10 19:46:11 +0800

    Linear Algebra

--- Experiments/Tensorflow/Math/linear_algebra.py
+++ Experiments/Tensorflow/Math/linear_algebra.py
@@ -8,16 +8,32 @@ Project: https://github.com/roatienza/Deep-Learning-Experiments/blob/master/Expe
 
 import tensorflow as tf
 import numpy as np
+import sys
 
-A = tf.Variable(np.array([ [1, 2, 3], [4, 5, 6], [7, 8, 9] ]))
-B = tf.ones([3,3])
-b = tf.fill([1,3], 2)
+A = tf.constant([ [1.,2.], [3.,4.] ])
+B = tf.fill([2,2], 2.)
+x = tf.ones([2,1])
+b = tf.fill([2,1], 2.)
+y1 = tf.matmul(A,x)
+y2 = tf.add(y1,b)
 
 # run within a session and print
 with tf.Session() as session:
 	print("Tensorflow version: " + tf.__version__)
 	tf.global_variables_initializer().run()
-	print("%s", A.eval())
-	print(B.eval())
-	print(b.eval())
-
+	print("y1 = A * x ")
+	sys.stdout.write(str(y1.eval()) + " = " + str(A.eval()) + " * " + str(x.eval()) + "\n")
+	print("y2 = A*x + b ")
+	sys.stdout.write(str(y2.eval()) + " = " + str(A.eval()) + " * " + str(x.eval()) + " + " + str(b.eval()) + "\n")
+	print("tran(A) = ")
+	print(tf.matrix_transpose(A).eval())
+	print("inv(A) = ")
+	print(tf.matrix_inverse(A).eval())
+	print("rank(A) = ")
+	print(tf.rank(A).eval())
+	print("A*inv(A) = Eye(2)")
+	print( tf.matmul(A,tf.matrix_inverse(A)).eval() )
+	print("elem(A)*elem(B) = ")
+	print(tf.mul(A,B).eval())
+	print("elem(A)+elem(B) = ")
+	print(tf.add(A,B).eval())
