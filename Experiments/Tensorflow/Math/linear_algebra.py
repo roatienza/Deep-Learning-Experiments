2d74eb52dca3a3a8611004a238d87819baa9a51f
Rowel Atienza <rowel.atienza@gmail.com>
2017-01-10 19:53:45 +0800

    added comments

--- Experiments/Tensorflow/Math/linear_algebra.py
+++ Experiments/Tensorflow/Math/linear_algebra.py
@@ -10,11 +10,21 @@ import tensorflow as tf
 import numpy as np
 import sys
 
+# Square matrix A of rank 2
 A = tf.constant([ [1.,2.], [3.,4.] ])
+
+# 2x2 Square matrix B will all elements 2.
 B = tf.fill([2,2], 2.)
+
 x = tf.ones([2,1])
+
+# 2x1 matrix will all elements equal to 2.0
 b = tf.fill([2,1], 2.)
+
+# matrix multiplication
 y1 = tf.matmul(A,x)
+
+# addition of matrix
 y2 = tf.add(y1,b)
 
 # run within a session and print
@@ -25,15 +35,27 @@ with tf.Session() as session:
 	sys.stdout.write(str(y1.eval()) + " = " + str(A.eval()) + " * " + str(x.eval()) + "\n")
 	print("y2 = A*x + b ")
 	sys.stdout.write(str(y2.eval()) + " = " + str(A.eval()) + " * " + str(x.eval()) + " + " + str(b.eval()) + "\n")
+
+	# transpose of A
 	print("tran(A) = ")
 	print(tf.matrix_transpose(A).eval())
+
+	# inverse of A
 	print("inv(A) = ")
 	print(tf.matrix_inverse(A).eval())
+
+	# rank of A
 	print("rank(A) = ")
 	print(tf.rank(A).eval())
+
+	# product of a matrix and its inverse is an identity (non-singular)
 	print("A*inv(A) = Eye(2)")
 	print( tf.matmul(A,tf.matrix_inverse(A)).eval() )
+
+	# element-wise multiplication
 	print("elem(A)*elem(B) = ")
 	print(tf.mul(A,B).eval())
+
+	# element-wise addition
 	print("elem(A)+elem(B) = ")
 	print(tf.add(A,B).eval())
