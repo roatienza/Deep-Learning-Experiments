db6afb6ad45cb3b4caa3c0c43a98c219f0acd6e2
Rowel Atienza <rowel.atienza@gmail.com>
2017-01-15 09:40:02 +0800

    Logical operations by NN

--- Experiments/Tensorflow/Neural_Networks/logic_gate_logits.py
+++ Experiments/Tensorflow/Neural_Networks/logic_gate_logits.py
@@ -1,9 +1,9 @@
 '''
-Logical Operation by 2-layer Neural Networks on TensorFlow
+Logical Operation by 2-layer Neural Networks (Logistic Regression) on TensorFlow
 Author: Rowel Atienza
 Project: https://github.com/roatienza/Deep-Learning-Experiments
 '''
-# On command line: python logic_gate.py
+# On command line: python logic_gate_logits.py
 # Prerequisite: tensorflow (see tensorflow.org)
 
 
@@ -12,11 +12,10 @@ import numpy as np
 
 learning_rate = 0.4
 x_data = np.reshape(np.array( [[0., 0.], [0., 1.], [1., 0.], [1., 1.]], dtype=np.float32 ),[4,2])
-
 # try other logics; xor = [0., 1., 1., 0.], or = [0., 1., 1., 1.], and = [0., 0., 0., 1.], etc
-logic_out = np.array([0., 1., 1.,0.], dtype=np.float32)
+logic_out = np.array([0., 1., 1., 0.], dtype=np.float32)
 y_data = np.reshape(logic_out,[4,1])
-# n = y_data.shape[0]
+n = y_data.shape[0]
 
 x = tf.placeholder(tf.float32, shape=(None, 2))
 y = tf.placeholder(tf.float32, shape=(None, 1))
@@ -31,8 +30,10 @@ b1 = tf.Variable(tf.zeros([1, 1]))
 
 hidden = tf.matmul(x, W0) + b0
 yp = tf.matmul(tf.nn.relu(hidden), W1) + b1
+logits = tf.nn.softmax(yp,dim=0)
 
-loss = tf.reduce_mean(tf.square(yp - y))
+entropy = -tf.mul(y,tf.log(logits))
+loss = tf.reduce_mean(entropy)
 optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate)
 train_step = optimizer.minimize(loss)
 
@@ -45,7 +46,7 @@ with tf.Session() as session:
         # feed_dict ={x:x_data[offset:(offset+2),:], y:y_data[offset:(offset+2)]}
         # so we use all data during training
         feed_dict = {x: x_data[:,:], y: y_data[:]}
-        _, l = session.run([train_step, loss],feed_dict=feed_dict)
+        _, l, y_, yp_ = session.run([train_step, loss, y, logits],feed_dict=feed_dict)
         if (i+1) % 100 == 0:
             print("--- %d: Loss = %lf" % (i+1, l))
     # Let's validate if we get the correct output given an input
@@ -55,5 +56,5 @@ with tf.Session() as session:
     print(input)
     hidden = tf.matmul(input, W0) + b0
     print("Predicted output:")
-    yp = tf.matmul(tf.nn.relu(hidden), W1) + b1
+    yp = tf.nn.softmax(tf.matmul(tf.nn.relu(hidden), W1) + b1,dim=0)
     print(print(1*np.greater(yp.eval(),0.25)))
