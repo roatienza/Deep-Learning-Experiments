9585731a5c46bfa484e9fd561407c3d62b3285f4
Rowel Atienza <rowel.atienza@gmail.com>
2017-02-09 23:37:28 +0800

    not mnist random shuffle of training data

--- Experiments/Tensorflow/Deep_Feedforward_Networks/mnist_a2j_mlp.py
+++ Experiments/Tensorflow/Deep_Feedforward_Networks/mnist_a2j_mlp.py
@@ -3,7 +3,7 @@ Deep Neural Networks on Not MNIST
 Author: Rowel Atienza
 Project: https://github.com/roatienza/Deep-Learning-Experiments
 """
-# On command line: python notmnist_mlp.py
+# On command line: python mnist_a2j_mlp.py
 # Prerequisite: tensorflow (see tensorflow.org)
 
 from __future__ import print_function
@@ -13,6 +13,7 @@ import tensorflow as tf
 import os
 import sys
 import tarfile
+import random
 
 from os import walk
 from os.path import join
@@ -47,7 +48,7 @@ def extract(filename):
         data_folders = [os.path.join(root, d)
                         for d in sorted(os.listdir(root)) if d != '.DS_Store']
     if len(data_folders) == num_classes:
-        print("Previously extracted files...")
+        print("Using previously extracted files...")
         print(data_folders)
         return data_folders
     tar = tarfile.open(filename)
@@ -64,28 +65,32 @@ def extract(filename):
     print(data_folders)
     return data_folders
 
+def getfiles_fromlist(dirs):
+    files = []
+    for dir in dirs:
+        files.extend([os.path.join(dir,f) for f in sorted(os.listdir(dir)) if f != '.DS_Store'])
+    return files
+
 train_folders = extract(train_filename)
 test_folders = extract(test_filename)
+train_files = np.array(getfiles_fromlist(train_folders))
+test_files = np.array(getfiles_fromlist(test_folders))
+random.shuffle(train_files)
 
-def read_dir_image_files(dirs):
+def read_image_files(files):
     imagelabels = []
     imagedata = []
-    for dir in dirs:
-        for (path, folders, files) in walk(dir):
-            print("Reading folder ", dir, "...")
-            files = [f for f in files if not f[0] == '.']
-            label =  ( np.arange(num_classes) == (ord(dir[-1])-ord('A')) ).astype(np.float32)
-            for f in files:
-                data = readfile(join(dir,f))
-                if (data.size > 0):
-                    imagelabels.append(label)
-                    imagedata.append(data)
-            break
-
+    for file in files:
+        parent_dir = os.path.dirname(file)
+        label =  (np.arange(num_classes) == (ord(parent_dir[-1])-ord('A')) ).astype(np.float32)
+        data = readfile(file)
+        if (data.size > 0):
+            imagelabels.append(label)
+            imagedata.append(data)
     return np.array(imagedata),np.array(imagelabels)
 
-train_dataset, train_labels = read_dir_image_files(train_folders)
-test_dataset, test_labels = read_dir_image_files(test_folders)
+train_dataset, train_labels = read_image_files(train_files)
+test_dataset, test_labels = read_image_files(test_files)
 
 num_labels =  train_labels.shape[1]
 image_size = train_dataset.shape[2]
@@ -98,8 +103,8 @@ print(train_labels.shape)
 print(test_dataset.shape)
 print(test_labels.shape)
 
-batch_size = 25000
-hidden_units = 1024+256
+batch_size = 512*4
+hidden_units = 512*24
 
 graph = tf.Graph()
 with graph.as_default():
@@ -141,9 +146,9 @@ def accuracy(predictions, labels):
     return (100.0 * np.sum(np.argmax(predictions, 1) == np.argmax(labels, 1)) /
             predictions.shape[0])
 
-num_steps = 20001
+num_steps = 10001
 
-with tf.Session(graph=graph) as session:
+with tf.Session(graph=graph,config=tf.ConfigProto(log_device_placement=True)) as session:
     tf.global_variables_initializer().run()
     print(tf.__version__)
     for step in range(num_steps):
@@ -163,4 +168,10 @@ with tf.Session(graph=graph) as session:
                                                           test_labels))
 
     print(np.rint(test_pred.eval()))
-    # print(np.rint(session.run(test_pred)))
+
+
+
+# Minibatch accuracy: 92.6%
+# Test accuracy: 93.5%
+# batch_size = 256
+# hidden_units = 512*16
