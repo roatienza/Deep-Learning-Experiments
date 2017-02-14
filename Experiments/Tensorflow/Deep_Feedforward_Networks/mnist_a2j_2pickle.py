6c7e4934d1627410de0129ef09d9243e7b88e139
Rowel Atienza <rowel.atienza@gmail.com>
2017-02-14 14:50:39 +0800

    added mnist_a2j_2pickle.py

---
+++ Experiments/Tensorflow/Deep_Feedforward_Networks/mnist_a2j_2pickle.py
@@ -0,0 +1,112 @@
+"""
+Download notMNIST and generate a pickle file
+Author: Rowel Atienza
+Project: https://github.com/roatienza/Deep-Learning-Experiments
+"""
+# On command line: python3 mnist_a2j_2pickle.py
+# Prerequisite: tensorflow (see tensorflow.org)
+
+from __future__ import print_function
+
+import numpy as np
+import pickle
+import os
+import sys
+import tarfile
+import random
+import matplotlib.image as img
+
+from os.path import join
+from six.moves.urllib.request import urlretrieve
+
+url = 'http://yaroslavvb.com/upload/notMNIST/'
+
+def maybe_download(filename, expected_bytes):
+    """Download a file if not present, and make sure it's the right size."""
+    if not os.path.exists(filename):
+        print('Downloading ', filename, " ...")
+        filename, _ = urlretrieve(url + filename, filename)
+        statinfo = os.stat(filename)
+        if statinfo.st_size == expected_bytes:
+            print('Found and verified', filename)
+        else:
+            raise Exception('Failed to verify' +
+                            filename + '. Can you get to it with a browser?')
+    else:
+        print('Found and verified', filename)
+    return filename
+
+def extract(filename):
+    root = os.path.splitext(os.path.splitext(filename)[0])[0]  # remove .tar.gz
+    data_folders = []
+    if os.path.exists(root):
+        data_folders = [os.path.join(root, d)
+                        for d in sorted(os.listdir(root)) if d != '.DS_Store']
+    if len(data_folders) == num_classes:
+        print("Using previously extracted files...")
+        print(data_folders)
+        return data_folders
+    tar = tarfile.open(filename)
+    print('Extracting data for %s. This may take a while. Please wait.' % root)
+    sys.stdout.flush()
+    tar.extractall()
+    tar.close()
+    data_folders = [os.path.join(root, d)
+                    for d in sorted(os.listdir(root)) if d != '.DS_Store']
+    if len(data_folders) != num_classes:
+        raise Exception(
+            'Expected %d folders, one per class. Found %d instead.' % (
+                num_classes, len(data_folders)))
+    print(data_folders)
+    return data_folders
+
+def getfiles_fromlist(dirs):
+    files = []
+    for dir in dirs:
+        files.extend([os.path.join(dir,f) for f in sorted(os.listdir(dir)) if f != '.DS_Store'])
+    return files
+
+def readfile(path):
+    try:
+        data = img.imread(path)
+        return data
+    except:
+        return np.array([])
+
+def read_image_files(files):
+    imagelabels = []
+    imagedata = []
+    for file in files:
+        parent_dir = os.path.dirname(file)
+        label =  (np.arange(num_classes) == (ord(parent_dir[-1])-ord('A')) ).astype(np.float32)
+        data = readfile(file)
+        if (data.size > 0):
+            imagelabels.append(label)
+            imagedata.append(data)
+    return np.array(imagedata),np.array(imagelabels)
+
+train_filename = maybe_download('notMNIST_large.tar.gz', 247336696)
+test_filename = maybe_download('notMNIST_small.tar.gz', 8458043)
+
+num_classes = 10
+
+train_folders = extract(train_filename)
+test_folders = extract(test_filename)
+
+train_files = np.array(getfiles_fromlist(train_folders))
+test_files = np.array(getfiles_fromlist(test_folders))
+random.shuffle(train_files)
+
+all_dataset, all_labels = read_image_files(train_files)
+test_dataset, test_labels = read_image_files(test_files)
+image_size = all_dataset.shape[2]
+
+all_dataset = all_dataset.reshape((-1,image_size*image_size)).astype(np.float32)
+test_dataset = test_dataset.reshape((-1,image_size*image_size)).astype(np.float32)
+
+data = { "test_labels" : test_labels, "all_labels" : all_labels, "test_dataset" : test_dataset,
+         "all_dataset" : all_dataset }
+
+pickle_file = open( "mnist_a2j.pickle", "wb" )
+pickle.dump( data, pickle_file )
+pickle_file.close()
--- Experiments/Tensorflow/Deep_Feedforward_Networks/mnist_a2j_mlp.py
+++ Experiments/Tensorflow/Deep_Feedforward_Networks/mnist_a2j_mlp.py
@@ -30,19 +30,20 @@ valid_dataset = all_dataset[split+1:,:]
 train_labels = all_labels[:split,:]
 valid_labels = all_labels[split+1:,:]
 
-print(train_dataset.shape)
-print(train_labels.shape)
-print(valid_dataset.shape)
-print(valid_labels.shape)
-print(test_dataset.shape)
-print(test_labels.shape)
+print("Training size: ", train_dataset.shape)
+print("Training labels: ", train_labels.shape)
+print("Validation size: ", valid_dataset.shape)
+print("Validation labels: ", valid_labels.shape)
+print("Test size: ", test_dataset.shape)
+print("Test labels: ", test_labels.shape)
 
 num_labels =  train_labels.shape[1]
 image_size = 28
-batch_size = 256
-hidden_units = 512*10
-learning_rate = 0.0001
+batch_size = 512
+hidden_units = 512*16
+learning_rate = 0.0002
 num_steps = 50001
+dropout = 0.8
 
 graph = tf.Graph()
 with graph.as_default():
@@ -55,15 +56,15 @@ with graph.as_default():
 
     weights1 = tf.Variable(
         tf.truncated_normal([image_size * image_size, hidden_units]))
-    biases1 = tf.add(tf.Variable(tf.zeros([hidden_units])),0.01)
+    biases1 = tf.add(tf.Variable(tf.zeros([hidden_units])),0.1)
 
     weights2 = tf.Variable(
         tf.truncated_normal([hidden_units, hidden_units]))
-    biases2 = tf.add(tf.Variable(tf.zeros([hidden_units])),0.01)
+    biases2 = tf.add(tf.Variable(tf.zeros([hidden_units])),0.1)
 
     weights3 = tf.Variable(
         tf.truncated_normal([hidden_units, num_labels]))
-    biases3 = tf.add(tf.Variable(tf.zeros([num_labels])),0.01)
+    biases3 = tf.add(tf.Variable(tf.zeros([num_labels])),0.1)
 
 # 3-layer with dropout at hidden layer
     def model(data,dropout=0.5):
@@ -76,7 +77,7 @@ with graph.as_default():
         logits = tf.add(tf.matmul(dropout2, weights3), biases3)
         return logits, tf.nn.softmax(logits)
 
-    train_logits, train_pred = model(tf_train_dataset,dropout=0.95)
+    train_logits, train_pred = model(tf_train_dataset,dropout=dropout)
     loss = tf.reduce_mean(
          tf.nn.softmax_cross_entropy_with_logits(train_logits, tf_train_labels))
     optimizer = tf.train.GradientDescentOptimizer(learning_rate=learning_rate).minimize(loss)
